b7d1f2ad310cdecd7feb59a8ea645e00b7f106d6
Jasper van der Linden <jappe@pop-os.localdomain>
2018-11-22 15:28:09 +0100

    fix: Use port provided by Heroku

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ class TailwindExtractor {
 
 module.exports = {
   server: {
-    port: process.env.NODE_ENV === 'production' ? 80 : 3000,
+    port: process.env.PORT || 3000,
     host: '0.0.0.0',
   },
 	
