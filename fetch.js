36252c20f07284a8333966dd67ebb3c32d3ae618
Jasper van der Linden <jappe@pop-os.localdomain>
2018-11-24 13:35:00 +0100

    chore: Add postbuild command to fetch latest tree

---
+++ fetch.js
@@ -0,0 +1,15 @@
+const axios = require('axios')
+const fs = require('fs')
+
+const GITHUB_URL = 'https://raw.githubusercontent.com/suitendaal/designtree/master/tree.json'
+
+const fetchTree = async url => {
+  const { data: tree } = await axios.get(url)
+
+  fs.writeFile('tree.json', JSON.stringify(tree), error => {
+    if (error) throw error
+    console.info('[INFO]: The new design tree has successfully been pushed.')
+  })
+}
+
+fetchTree(GITHUB_URL)
